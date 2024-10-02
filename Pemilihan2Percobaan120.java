d56f5198b34054659be786c7af12708012724926
rojalingga <rojalingga2006@gmail.com>
2024-10-02 20:49:55 +0700

    Modifikasi jika kelipatan 100 dan 400 juga tahun kabisat

--- Pemilihan2Percobaan120.java
+++ Pemilihan2Percobaan120.java
@@ -12,10 +12,15 @@ public class Pemilihan2Percobaan120 {
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0){
                 System.out.println("Tahun Kabisat");
-            } else {
-                System.out.println("Bukan Tahun Kabisat");
-            }
-
+            } else if ((tahun % 100) == 0){
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            } 
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
         }
     }
 }
